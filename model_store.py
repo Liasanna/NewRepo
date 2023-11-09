293dd00dfcc84450173cb119639ef96efd135cfd
Liasanna <112963336+Liasanna@users.noreply.github.com>
2023-11-09 08:59:25 +0300

    Create model_store.py

---
+++ model_store.py
@@ -0,0 +1,30 @@
+from service import Point 3D
+
+class Texture:
+  pass
+
+class Poligon:
+    def __init__(self, point):
+      self.points = [point]
+
+class PoligonalModel:
+   def __init__(self, textures):
+     self.Poligons = []
+     self.Textures = textures
+     self.Poligons.append(Poligon(Point3D)()))
+
+class Flash:
+  def __init__(self):
+    self.Location = Nome
+     self.Angle = Nome
+     self.Color = Nome
+     self.Power = Nome
+
+    def Rotate(self, angleAction):
+      pass
+
+    def Move(self, pointAction):
+      pass
+    
+
+     
